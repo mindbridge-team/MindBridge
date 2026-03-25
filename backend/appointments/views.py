cf7ad16c2464015b56b081e0082dab89f5a0b437
umerselteq <umerriasat06@gmail.com>
2026-03-25 02:30:36 -0500

    appointments

--- backend/appointments/views.py
+++ backend/appointments/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Appointment
+from .serializers import AppointmentSerializer
 
-# Create your views here.
+
+class CreateAppointmentView(generics.CreateAPIView):
+    serializer_class = AppointmentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(patient=self.request.user)
+
+
+class MyAppointmentsView(generics.ListAPIView):
+    serializer_class = AppointmentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Appointment.objects.filter(patient=self.request.user)
+
+
+class CounsellorAppointmentsView(generics.ListAPIView):
+    serializer_class = AppointmentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Appointment.objects.filter(counsellor__user=self.request.user)
+
+
+class UpdateAppointmentStatusView(generics.UpdateAPIView):
+    queryset = Appointment.objects.all()
+    serializer_class = AppointmentSerializer
+    permission_classes = [permissions.IsAuthenticated]
