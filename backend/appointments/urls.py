00b66991d261899b4a08c0e8cf94730d9c97413a
umerselteq <umerriasat06@gmail.com>
2026-03-26 13:39:28 -0500

    slots-availability

--- backend/appointments/urls.py
+++ backend/appointments/urls.py
@@ -4,11 +4,14 @@ from .views import (
     MyAppointmentsView,
     CounsellorAppointmentsView,
     UpdateAppointmentStatusView,
+    AvailableSlotsView,
 )
+
 
 urlpatterns = [
     path("create/", CreateAppointmentView.as_view(), name="create-appointment"),
     path("my/", MyAppointmentsView.as_view(), name="my-appointments"),
     path("counsellor/", CounsellorAppointmentsView.as_view(), name="counsellor-appointments"),
     path("<int:pk>/", UpdateAppointmentStatusView.as_view(), name="update-appointment"),
+    path("available/<int:counsellor_id>/", AvailableSlotsView.as_view(), name="available-slots"),
 ]
